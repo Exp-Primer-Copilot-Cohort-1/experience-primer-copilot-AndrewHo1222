bdc747e126a3e6e78680c4b1e29e80a54ef9074c
AndrewHo <48797504+AndrewHo1222@users.noreply.github.com>
2024-01-09 15:23:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,60 @@
-// 引入 http 模組
-const http = require('http');
+// Create web server with express
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-// 建立伺服器
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+// Set up body-parser
+app.use(bodyParser.json());
+
+// Set up comments
+const comments = new Map();
+comments.set(1, ['Nice post', 'Thanks for sharing']);
+comments.set(2, ['Good job']);
+
+// Get all comments
+app.get('/comments', (req, res) => {
+  res.send(comments);
 });
 
-// 監聽 3000 port
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+// Get comments by post id
+app.get('/posts/:id/comments', (req, res) => {
+  const postId = Number(req.params.id);
+  const postComments = comments.get(postId);
+  res.send(postComments);
+});
+
+// Add new comment
+app.post('/posts/:id/comments', (req, res) => {
+  const postId = Number(req.params.id);
+  const newComment = req.body.comment;
+  const postComments = comments.get(postId);
+  postComments.push(newComment);
+  comments.set(postId, postComments);
+  res.send(postComments);
+});
+
+// Delete comment
+app.delete('/posts/:id/comments/:commentId', (req, res) => {
+  const postId = Number(req.params.id);
+  const commentId = Number(req.params.commentId);
+  const postComments = comments.get(postId);
+  const filteredComments = postComments.filter((comment, index) => index !== commentId);
+  comments.set(postId, filteredComments);
+  res.send(filteredComments);
+});
+
+// Update comment
+app.put('/posts/:id/comments/:commentId', (req, res) => {
+  const postId = Number(req.params.id);
+  const commentId = Number(req.params.commentId);
+  const newComment = req.body.comment;
+  const postComments = comments.get(postId);
+  postComments[commentId] = newComment;
+  comments.set(postId, postComments);
+  res.send(postComments);
+});
+
+// Listen to port 3000
+app.listen(3000, () => {
+  console.log('Web server is listening on port 3000!');
 });
